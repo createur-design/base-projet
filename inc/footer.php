bd93f900a28ec7b7825ef9fc1bdd018957dee05f
cdelobel.ext@simplon.co <christophe@createur-design.fr>
2019-03-05 21:53:02 +0100

    add foundation.min.css

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,6 @@
     </main>
     <footer></footer>
+    <link rel="stylesheet" href="css/foundation.min.css">
     <link rel="stylesheet" href="css/app.min.css">
     <script src="js/app.min.js"></script>
 </body>
--- index.php
+++ index.php
@@ -11,8 +11,12 @@ include 'inc/header.php';
 
 <section class="hero">
 
-    <h1>Titre unique de la page</h1>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum, obcaecati nemo. Fugiat nihil, ut laboriosam similique sit enim quia quas tempora amet delectus quo iste labore deserunt dolores laborum sapiente.</p>
+    <div class="row">
+        <div class="small-12 columns">
+            <h1>Titre unique de la page</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum, obcaecati nemo. Fugiat nihil, ut laboriosam similique sit enim quia quas tempora amet delectus quo iste labore deserunt dolores laborum sapiente.</p>
+        </div>
+    </div>    
 
 </section>
 

24d1be2156da79699eacd8d787d943fcc90c65b9
cdelobel.ext@simplon.co <christophe@createur-design.fr>
2019-03-06 11:27:52 +0100

    update

--- js/app.js
+++ js/app.js
@@ -9,16 +9,7 @@ window.addEventListener("load", function(){
 // Burger Menu Simply Basic
 var btMenu = document.getElementById('btMenu');
 var nav = document.getElementById('nav');
-var visibleMenu = false;
 btMenu.addEventListener("click", function (){
-    console.log('click menu');
-    if(visibleMenu === false){
-        nav.classList.add("show");
-        visibleMenu = true;
-    }
-    else{
-        nav.classList.remove("show");
-        visibleMenu = false;
-    }
+    nav.classList.toggle("show");
 });
 // Burger Menu
